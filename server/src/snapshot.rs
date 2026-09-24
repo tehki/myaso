5a11c58b363f206f214d4b852909fee759a11b6e
tehki <ilya.von.gruntal@gmail.com>
2026-09-24 14:19:47 +0300

    M87: apply rustfmt to replication preallocation regression

--- server/src/snapshot.rs
+++ server/src/snapshot.rs
@@ -226,8 +226,7 @@ pub struct ReplicationFrame {
 impl ReplicationFrame {
     pub fn from_fighters(server_tick: u32, fighters: &[Fighter]) -> Self {
         let mut states = Vec::with_capacity(fighters.len());
-        let mut cells: HashMap<(i32, i32), Vec<usize>> =
-            HashMap::with_capacity(fighters.len());
+        let mut cells: HashMap<(i32, i32), Vec<usize>> = HashMap::with_capacity(fighters.len());
         for fighter in fighters {
             let state = WireEntity::from_fighter(fighter);
             debug_assert!(
@@ -1412,12 +1411,7 @@ mod tests {
         for net_id in 1..=64 {
             let column = ((net_id - 1) % 8) as f32;
             let row = ((net_id - 1) / 8) as f32;
-            assert!(world.add_player_at(
-                net_id,
-                600.0 + column * 550.0,
-                600.0 + row * 550.0,
-                0.0,
-            ));
+            assert!(world.add_player_at(net_id, 600.0 + column * 550.0, 600.0 + row * 550.0, 0.0,));
         }
 
         let fighter_count = world.fighters().len();
